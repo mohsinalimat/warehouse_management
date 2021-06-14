58286be90ef4b9155ac6ea2c3e90841eeef743ed
Jeans K. Real <jkrb7@hotmail.com>
2021-06-13 21:58:26 -0600

    Warehouse Receipt Doctype

---
+++ warehouse_management/warehouse_management/doctype/warehouse_receipt/warehouse_receipt.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Agile Shift I/o and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Warehouse Receipt', {
+	// refresh: function(frm) {
+
+	// }
+});
