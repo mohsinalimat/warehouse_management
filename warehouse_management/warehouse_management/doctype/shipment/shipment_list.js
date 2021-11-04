5a79ad73d2387d5af4137187dff9d9d6c4237889
Jeans K. Real <jkrb7@hotmail.com>
2021-11-04 14:25:23 -0600

    Style improvement

--- warehouse_management/warehouse_management/doctype/shipment/shipment_list.js
+++ warehouse_management/warehouse_management/doctype/shipment/shipment_list.js
@@ -7,11 +7,20 @@ frappe.listview_settings['Shipment'] = {
 
     get_indicator(doc) {
         const status_color = {
-            'Open': 'blue',
+            'Open': 'lightblue',
             'Closed': 'green'
         };
 
         return [__(doc.status), status_color[doc.status], 'status,=,' + doc.status];
     },
+    formatters: {
+        transportation_type(val) {
+            let color = (val === 'Sea') ? 'blue' : 'red';
+            return `<span class="indicator-pill ${color} filterable ellipsis"
+                data-filter="transportation_type,=,${frappe.utils.escape_html(val)}">
+				<span class="ellipsis"> ${val} </span>
+			<span>`;
+        }
+    }
 
 }
