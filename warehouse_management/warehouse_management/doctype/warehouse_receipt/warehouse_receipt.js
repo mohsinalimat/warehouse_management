e40c8ca6720b4d0ed81a0933c142b453a1935a87
Jeans K. Real <jkrb7@hotmail.com>
2021-12-13 11:17:20 -0600

    WIP

--- warehouse_management/warehouse_management/doctype/warehouse_receipt/warehouse_receipt.js
+++ warehouse_management/warehouse_management/doctype/warehouse_receipt/warehouse_receipt.js
@@ -3,25 +3,27 @@ frappe.ui.form.on('Warehouse Receipt', {
         $('.layout-side-section').hide(); // Little Trick to work better
 
         // https://stackoverflow.com/a/1977126/3172310
-        $(document).on('keydown', "input[data-fieldname='tracking_number']", (event) => {
-            if (event.key === 'Enter') {  // Enter key is sent if field is set from barcode scanner.
-                event.preventDefault(); // We prevent the button 'Attach Image' opens a pop-up.
-            }
-        });
+        $(document).on('keydown', "input[data-fieldname='tracking_number'], input[data-fieldname='weight'], " +
+            "input[data-fieldname='length'], input[data-fieldname='width'], input[data-fieldname='height']", (event) => {
+                if (event.key === 'Enter') {  // Enter key is sent if field is set from barcode scanner.
+                    event.preventDefault(); // We prevent the button 'Attach Image' opens a pop-up.
+                }
+            });
     },
 
-    shipping_label_photo: function (frm) {
-        if (!frm.is_new()) return;
+    before_save: function (frm) {
+        frm.print_label = frm.is_new(); // If new true else undefined.
+    },
 
-        frm.save().then(() => {  // Is new doc and photo is added, Doc is saved and printed.
-            window.open(
-                frappe.urllib.get_full_url(
-                    'printview?doctype=Warehouse%20Receipt&' +
-                    'name=' + frm.doc.name +
-                    '&trigger_print=1&format=Warehouse%20Receipt%20Labels&no_letterhead=1&letterhead=No%20Letterhead&settings=%7B%7D&_lang=es'
-                ) // Emulating print.js inside frappe/printing/page/print/print.js
-            );
-        });
+    after_save: function (frm) {
+        if (!frm.print_label) return;
+
+        window.open(
+            frappe.urllib.get_full_url(
+                'printview?doctype=Warehouse%20Receipt&name=' + frm.doc.name +
+                '&trigger_print=1&format=Warehouse%20Receipt%20Labels&no_letterhead=1&letterhead=No%20Letterhead&settings=%7B%7D&_lang=es'
+            ) // Emulating print.js inside frappe/printing/page/print/print.js
+        );
     }
 });
 
